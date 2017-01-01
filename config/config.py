575290533d3e46c2b5e0ebae071226fdafe3c1c0
developer-account <irteam@tcnpiapp-91a901.svr.toastmaker.net>
2017-01-01 16:02:44 +0900

    add icon url

--- config/config.py
+++ config/config.py
@@ -13,12 +13,21 @@ ICON_URL = [
     "https://2.bp.blogspot.com/-mBtbKi2d3BI/WBW9DKxhfXI/AAAAAAABbEY/gqySwEOHVCc0Krxu5YQkv9LSRJGhSjdkwCLcB/s0/%25EC%259D%258C%25EC%2595%2585%25EC%25A4%2591%25EC%258B%25AC%2B%25ED%258A%25B8%25EC%2599%2580%25EC%259D%25B4%25EC%258A%25A4%2B%25EC%259A%25B8%25EB%25B3%25B4%2B%25EC%25B6%25A4%2B%25EC%259B%2580%25EC%25A7%25A4%2B%25282%2529.gif",
     "http://image.fmkorea.com/files/attach/new/20160728/3655109/81379802/424399056/f68e76875e0fd0f18e33612c52949dbd.gif",
     "http://image.fmkorea.com/files/attach/new/20160728/3655109/81379802/424399056/8b324120f8a59900a1c1fbae20d97f3a.gif",
-    "http://cfile25.uf.tistory.com/image/220C234E583EFA6E055F2E"
+    "http://cfile25.uf.tistory.com/image/220C234E583EFA6E055F2E",
+    "http://cfile26.uf.tistory.com/image/234A114D58612650265B49",
+    "http://cfile25.uf.tistory.com/image/22444A4E586508B00BA5FC",
+    "http://cfile4.uf.tistory.com/image/2152D54158650B4613E440",
+    "http://cfile26.uf.tistory.com/image/2130283E55BD4904232937",
+    "http://cfile5.uf.tistory.com/image/2539063E55BD490D209ADF",
+    "http://cfile30.uf.tistory.com/image/26715E4955FB75E6046370",
+    "http://cfile10.uf.tistory.com/image/243E0035558961C50D1024",
+    "http://cfile26.uf.tistory.com/image/23399935558961D1119880"
 ]
 
 CHAT_HOOK_URL = [
     "http://hook.dooray.com/services/1387695619080878080/1800613728333582805/60OMP3X4R1WHjcI0bGZvgQ", #friends with soju
-    "http://hook.dooray.com/services/1387695619080878080/1800538452602198003/99NKxr3eSpCtUoUEoDl-jA" #gwiyeong bot
+    "http://hook.dooray.com/services/1387695619080878080/1800538452602198003/99NKxr3eSpCtUoUEoDl-jA", #gwiyeong bot
+    "https://hook.dooray.com/services/1387695619080878080/1851132032137964071/BnGA4EFcQ5uzb4WF8TbOgQ" # from jinwoo lee; 
 ]
 
 STOCK_CHAT_HOOK_URL = [
